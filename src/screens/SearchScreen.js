b19e32a11096423ab3a4dba996e6a29a4f4b90f0
tysonlander <tysonlander@gmail.com>
2019-11-18 21:20:09 -0700

    searchbar setup with api

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
+
+const SearchScreen = () => {
+  const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('')
+
+  const searchApi = async (searchTerm) => {
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: 'san jose'
+        }
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage('Something went wrong')
+    }
+  };
+
+  return (
+    <View>
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+
+      <Text>Search Screen</Text>
+      <Text>We have found {results.length} results</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
